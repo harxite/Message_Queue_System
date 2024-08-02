ea38e004242701a88dfd7adb296afd40f68e748c
arxite <azizogluharun@gmail.com>
2024-08-02 16:27:46 +0300

    INIT

---
+++ MessageQueueSystem/MessageQueueSystem/Controllers/MessagesController.cs
@@ -0,0 +1,21 @@
+﻿using MessageQueueSystem.Services;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MessagesController : ControllerBase
+{
+    private readonly IMessageQueueService _messageQueueService;
+
+    public MessagesController(IMessageQueueService messageQueueService)
+    {
+        _messageQueueService = messageQueueService;
+    }
+
+    [HttpPost]
+    public IActionResult Post([FromBody] string message)
+    {
+        _messageQueueService.SendMessage(message);
+        return Ok("Message sent to queue");
+    }
+}
---
+++ MessageQueueSystem/MessageQueueSystem/Services/RabbitMQService.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace MessageQueueSystem.Services
+{
+    public interface IMessageQueueService
+    {
+        void SendMessage(string message);
+    }
+
+    public class RabbitMQService : IMessageQueueService
+    {
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+
+        public RabbitMQService()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.QueueDeclare(queue: "message_queue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+        }
+
+        public void SendMessage(string message)
+        {
+            var body = Encoding.UTF8.GetBytes(message);
+            _channel.BasicPublish(exchange: "", routingKey: "message_queue", basicProperties: null, body: body);
+        }
+    }
+}
